f452e48f5148ef299e0eff661d3bea38de804438
Jenny Nguyen <40232531+BanhBaozi@users.noreply.github.com>
2018-11-30 17:12:05 -0800

    Create DebugTen3.java

---
+++ Debugging/10-3/DebugTen3.java
@@ -0,0 +1,19 @@
+// A PhoneBook is a Book with a city
+import java.util.*;
+public class DebugTen3
+{
+   public static void main(String[] args)
+   {
+      String area, inStr;
+       int pages;
+      Scanner input = new Scanner(System.in);
+
+      System.out.println("Enter city for phone book");
+      area = input.next();
+      System.out.println("Enter number of pages in " + area + "'s phone book");
+      inStr = input.next();
+      pages = Integer.parseInt(inStr);
+      DebugPhoneBook pb = new DebugPhoneBook(pages, area);
+      pb.display();
+   }
+}
